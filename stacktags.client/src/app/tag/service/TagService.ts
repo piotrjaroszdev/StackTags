acefc12ea211b05b3f0bbb67e30db2c53716d817
LAPTOP\japit <piotr.jarosz.dev@gmail.com>
2025-10-07 15:26:38 +0200

    environment file has been added

--- stacktags.client/src/app/tag/service/TagService.ts
+++ stacktags.client/src/app/tag/service/TagService.ts
@@ -2,10 +2,11 @@ import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Tag } from "../model/Tag";
 import { Observable } from "rxjs";
+import { environment } from "../../../environments/environment";
 
 @Injectable({ providedIn: 'root' })
 export class TagService {
-  private apiUrl = 'http://localhost:5043/api/tags';
+  private apiUrl = `${environment.apiUrl}/tags`;
 
   constructor(private http: HttpClient) {}
 
---
+++ stacktags.client/src/environments/environment.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: false,
+  //local
+  apiUrl: 'http://localhost:5043/api'
+  //docker
+  //apiUrl: 'http://localhost:8080/api'
+};
